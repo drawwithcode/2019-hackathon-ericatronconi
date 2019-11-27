c436bd643b4d14cd3dd191dbcf624f7b02f87525
ericatronconi <erica.tronconi@gmail.com>
2019-11-27 10:42:24 +0100

    first

--- sketch.js
+++ sketch.js
@@ -1,11 +1,30 @@
+var myImage;
+var mySong;
+
 function preload(){
-  // put preload code here
+myImage = loadImage("./assets/tg1_logo.png");
+mySong1 = loadSound("./assets/TG1_bumper.mp3");
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(0);
 }
 
 function draw() {
-  // put drawing code here
+  imageMode(CENTER);
+  image(myImage, windowWidth/2, windowHeight/2, myImage.width/2, myImage.height/2);
+}
+
+function keyPressed() {
+  if(keyCode == RIGHT_ARROW) {
+    mySong.play();
+  } else if(keyCode == LEFT_ARROW) {
+    mySong.playMode('restart');
+    mySong.play();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
